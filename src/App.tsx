0189f4abc576e9133c7e5a76b303d749cff106c4
grhg <52474584+tgrhg@users.noreply.github.com>
2024-12-12 12:47:57 +0900

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Key Usage Sample</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Count UP!
+        </button>
+        {/* key プロパティを指定 */}
+        <CountMonitor key={`counter-${count}`} count={count} />
+      </div>
+    </>
+  )
+}
+
+// これは実際には外部ライブラリ内のコード 
+const CountMonitor = (props: { count: number }) => {
+  const displayNumber = useState(props.count)
+
+  return (<>
+    <div className="card">
+      現在のカウントは「<>{displayNumber}</>」です。
+    </div>
+  </>)
+}
+
+export default App
