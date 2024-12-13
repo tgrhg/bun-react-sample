43e711fe95a0eb2577e4eaf8ccd6d3c7cf7a75fa
grhg <52474584+tgrhg@users.noreply.github.com>
2024-12-13 23:58:20 +0900

    :construction_worker: fix tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+})
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await expect(
+    page.getByRole('heading', { name: 'Key Usage Sample' })
+  ).toBeVisible()
+})
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test('count up', async ({ page }) => {
+  await expect(page.getByText('現在のカウントは「0」です。')).toBeVisible()
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.getByRole('button', { name: 'Count UP!' }).click()
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  await expect(page.getByText('現在のカウントは「1」です。')).toBeVisible()
+})
